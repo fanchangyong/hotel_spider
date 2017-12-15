dbf54a497a145fa8385c053fc4589b849ddb277b
fanchangyong <fanchangyong@gmail.com>
2017-12-15 13:52:26 +0800

    Add pagination for ctrip_intl

--- hotel_spider/spiders/ctrip_intl.py
+++ hotel_spider/spiders/ctrip_intl.py
@@ -8,11 +8,10 @@ from hotel_spider.items import ProductItem
 
 class CtripIntlSpider(scrapy.Spider):
     name = 'ctrip_intl'
-    allowed_domains = ['ctrip.com']
     start_urls = ['http://hotels.ctrip.com/international/landmarks/']
 
     def parse(self, response):
-        for country in response.css('ul.nation_list li'):
+        for country in response.css('ul.nation_list li')[:1]:
             country_name = country.css('strong.nation a::text').extract_first()
             country_url = country.css('strong.nation a::attr(href)').extract_first()
             country_url = country_url + '/city'
@@ -21,14 +20,85 @@ class CtripIntlSpider(scrapy.Spider):
             yield request
 
     def parse_country_page(self, response):
-        for city in response.css('ul.other_city li a'):
+        for city in response.css('ul.other_city li a')[:1]:
             city_name = city.css('::text').extract_first()
             city_name = city_name.replace('酒店', '')
             city_url = city.css('::attr(href)').extract_first()
             city_url = 'http://hotels.ctrip.com' + city_url
-            request = Request(url=city_url, callback=self.parse_hotel_list_page)
+
+            # Get max_page number
+            script = """
+            function wait_for_element(splash, ele_selector)
+                while true do
+                    local ele = splash:select(ele_selector)
+                    if ele then
+                        break
+                    end
+                    splash:wait(0.05)
+                end
+            end
+            function main(splash, args)
+                local url = args.url
+                assert(splash:go(url))
+                splash:wait(5)
+                wait_for_element(splash, 'div.c_page_list')
+                local page_links = splash:select_all('.c_page_list a')
+                local max_page = 0
+                for _, page_link in ipairs(page_links) do
+                    local page = tonumber(page_link:text())
+                    if page > max_page then
+                        max_page = page
+                    end
+                end
+                return { max_page=max_page }
+            end
+            """
+            request = SplashRequest(callback=self.parse_after_max_page, endpoint='/execute',
+                                    args={
+                                        'lua_source': script,
+                                        'url': city_url,
+                                        'images': 0,
+                                        'headers': {
+                                            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+                                        }
+                                    })
             request.meta['country'] = response.meta['country']
             request.meta['city'] = city_name
+            yield request
+
+    def parse_after_max_page(self, response):
+        max_page = response.data['max_page']
+        base_url = response.url
+        for page in range(1, max_page + 1):
+            script = """
+            function wait_for_element(splash, ele_selector)
+                while true do
+                    local ele = splash:select(ele_selector)
+                    if ele then
+                        break
+                    end
+                    splash:wait(0.05)
+                end
+            end
+            function main(splash, args)
+                local url = args.url
+                assert(splash:go(url))
+                wait_for_element(splash, '.hlist .hlist_item')
+                return splash:html()
+            end
+            """
+            paged_url = base_url + '/p' + str(page)
+            request = SplashRequest(endpoint='/execute', callback=self.parse_hotel_list_page,
+                                    args={
+                                        'url': paged_url,
+                                        'lua_source': script,
+                                        'images': 0,
+                                        'headers': {
+                                            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+                                        }
+                                    })
+            request.meta['country'] = response.meta['country']
+            request.meta['city'] = response.meta['city']
             yield request
 
     def parse_hotel_list_page(self, response):
@@ -36,10 +106,31 @@ class CtripIntlSpider(scrapy.Spider):
             hotel_name = hotel.css('.hlist_item_name a::text').extract_first()
             hotel_url = hotel.css('.hlist_item_name a::attr(href)').extract_first()
             hotel_url = 'http://hotels.ctrip.com' + hotel_url
-            request = SplashRequest(url=hotel_url, callback=self.parse_hotel_detail_page,
+
+            script = """
+            function wait_for_element(splash, ele_selector)
+                while true do
+                    local ele = splash:select(ele_selector)
+                    if ele then
+                        break
+                    end
+                    splash:wait(0.05)
+                end
+            end
+
+            function main(splash, args)
+                local url = args.url
+                assert(splash:go(url))
+                wait_for_element(splash, '.hroom_list .hroom_tr')
+                return splash:html()
+            end
+            """
+
+            request = SplashRequest(endpoint='/execute', callback=self.parse_hotel_detail_page,
                                     args={
-                                        'wait': 5,
-                                        'timeout': 20,
+                                        'url': hotel_url,
+                                        'lua_source': script,
+                                        'images': 0,
                                         'headers': {
                                             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
                                         }
@@ -51,13 +142,15 @@ class CtripIntlSpider(scrapy.Spider):
             yield request
 
     def parse_hotel_detail_page(self, response):
-        rooms = response.css('table#J_RoomListTbl tr[expand]')
         source = 'ctrip-intl'
         meta = response.meta
         country = meta['country']
         city = meta['city']
         hotel_name = meta['hotel_name']
         hotel_url = meta['hotel_url']
+
+        latitude = response.css('meta[itemprop="latitude"]::attr(content)').extract_first()
+        longitude = response.css('meta[itemprop="longitude"]::attr(content)').extract_first()
 
         for room in response.css('.hroom_list .hroom_tr'):
             room_name = room.css('.hroom_base .hroom_base_tit::text').extract_first()
@@ -69,6 +162,8 @@ class CtripIntlSpider(scrapy.Spider):
                 item['source'] = source
                 item['country'] = country
                 item['city'] = city
+                item['latitude'] = latitude
+                item['longitude'] = longitude
                 item['hotel_name'] = hotel_name
                 item['hotel_url'] = hotel_url
                 item['room_name'] = room_name
